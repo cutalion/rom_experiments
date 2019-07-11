8947aabbccf98dc7d2ce3d89ecd3fbd951c7f927
Alexander Glushkov <cutalion@gmail.com>
2019-07-11 23:56:45 +0300

    Update pg_range example

--- pg_range.rb
+++ pg_range.rb
@@ -9,19 +9,16 @@ conn.drop_table?(:pg_ranges)
 
 conn.create_table(:pg_ranges) do
   primary_key :id
-  daterange :range
+  tstzrange :range
 end
 
 config.relation(:pg_ranges) do
-  schema(:pg_ranges, infer: true)
+  schema(:pg_ranges, infer: true) do
+    attribute :range, ROM::SQL::Types::PG::TsTzRange
+  end
 end
 
 rom = ROM.container(config)
 pg_ranges = rom.relations.pg_ranges
 
-record = pg_ranges.changeset(:create, range: ROM::SQL::Postgres::Values::Range.new(Date.today, Date.today + 10, :'[]')).commit
-puts pg_ranges.where(range: record[:range]).to_a.inspect # works
-puts Hash[pg_ranges.where(range: record[:range]).one].inspect # works
-puts pg_ranges.where { range.overlap(record[:range]) }.to_a.inspect # does not work
-
-# rom.relations.pg_ranges.changeset(:create, range: Date.today..(Date.today + 10)).commit
+record = pg_ranges.changeset(:create, range: Time.now..(Time.now + 10)).commit
