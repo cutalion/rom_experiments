fdae88c28d7f5b59ccf9a40de8a4fc0916b9c2c5
Alexander Glushkov <cutalion@gmail.com>
2019-08-15 19:27:49 +0300

    Update pg_range examples with different range objects

--- pg_range.rb
+++ pg_range.rb
@@ -2,6 +2,20 @@ require 'rom'
 require 'rom-sql'
 require 'pry'
 
+class CustomRange < Range
+  def exclude_begin?
+    false
+  end
+
+  def lower
+    self.begin
+  end
+
+  def upper
+    self.end
+  end
+end
+
 config = ROM::Configuration.new(:sql, 'postgres://localhost/rom_experiments')
 conn = config.gateways[:default].connection
 
@@ -21,4 +35,22 @@ end
 rom = ROM.container(config)
 pg_ranges = rom.relations.pg_ranges
 
-record = pg_ranges.changeset(:create, range: Time.now..(Time.now + 10)).commit
+ruby_range = Range.new(Time.now, Time.now)
+rom_range = ROM::SQL::Postgres::Values::Range.new(Time.now, Time.now)
+sequel_range = Sequel::Postgres::PGRange.new(Time.now, Time.now)
+custom_range = CustomRange.new(Time.now, Time.now)
+
+def try(name)
+  yield
+  puts "#{name} works"
+  rescue => e
+    puts "#{name} does not work"
+    puts e.message
+  ensure
+    puts
+end
+
+try(:ruby_range) { pg_ranges.changeset(:create, range: ruby_range).commit }
+try(:sequel_range) { pg_ranges.changeset(:create, range: sequel_range).commit }
+try(:rom_range) { pg_ranges.changeset(:create, range: rom_range).commit }
+try(:custom_range) { pg_ranges.changeset(:create, range: custom_range).commit }
