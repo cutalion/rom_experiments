6732fe96ecb8408a1241b7a54c1eed420d07c1b8
Alexander Glushkov <cutalion@gmail.com>
2022-08-09 19:57:03 +0300

    Namespace example

---
+++ namespace.rb
@@ -0,0 +1,65 @@
+require 'rom'
+require 'rom-repository'
+require 'rom-sql'
+require 'logger'
+require 'pry'
+
+config = ROM::Configuration.new(:sql, 'sqlite::memory')
+
+conn = config.gateways[:default].connection
+config.gateways[:default].use_logger(Logger.new($stdout))
+
+conn.create_table(:users) do
+  primary_key :id
+  column :name, String
+end
+
+conn.create_table(:addresses) do
+  foreign_key :user_id, :users, null: false
+  column :street, String
+end
+
+module Entities
+end
+
+class Entities::User < ROM::Struct
+  def main_address
+    addresses.first
+  end
+end
+
+config.relation(:users) do
+  auto_struct true
+  struct_namespace Entities
+
+  schema(:users, infer: true) do
+    associations do
+      has_many :addresses
+    end
+  end
+end
+
+config.relation(:addresses) do
+  auto_struct true
+  struct_namespace Entities
+
+  schema(:addresses, infer: true) do
+    associations do
+      belongs_to :user
+    end
+  end
+end
+
+rom = ROM.container(config)
+
+users = rom.relations[:users]
+addresses = rom.relations[:addresses]
+
+alex = users.changeset(:create, name: 'Alex').commit
+address1 = addresses.changeset(:create, street: 'Street 1', user_id: alex.id).commit
+address2 = addresses.changeset(:create, street: 'Street 2', user_id: alex.id).commit
+
+puts users.combine(:addresses).first.main_address.street
+
+binding.pry
+1
